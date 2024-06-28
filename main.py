9edaca31f5f70e149234386338ec25b18e1ef2ab
Dan Cristian Deac <79625820+dandeac26@users.noreply.github.com>
2024-06-28 22:14:29 +0300

    middleware python service

--- main.py
+++ main.py
@@ -21,10 +21,19 @@ class Order(BaseModel):
     completionTime: str
     price: float
 
+
 class OrderDetail(BaseModel):
     orderId: str
     productId: str
     quantity: int
+
+
+class SensorData(BaseModel):
+    sensorId: str
+    temperature: float
+    humidity: float
+    timestamp: str
+
 
 @app.get("/orders")
 async def read_orders():
@@ -32,7 +41,7 @@ async def read_orders():
         response = httpx.get('http://localhost:8080/orders')
         logging.info(f"Data-api response status: {response.status_code}, text: {response.text}")
         return response.json()
- 
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -63,6 +72,24 @@ async def create_order(order: Order, background_tasks: BackgroundTasks):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@app.post("/notification/sensor-alert")
+async def create_alert(sensorData: SensorData):
+    try:
+        sensor_name = "Unknown Sensor"
+        if sensorData.sensorId == "9920ccbf-d43e-4713-bc6d-5460375f6e81":
+            sensor_name = "Warehouse1 Sensor"
+        elif sensorData.sensorId == "1920ccbf-d43e-4713-bc6d-5460375f6e82":
+            sensor_name = "Kitchen1 Sensor"
+
+        for websocket in websockets:
+            await websocket.send_text("Alert: " + sensor_name + " has temperature: " +
+                                      str(sensorData.temperature)[:2] + "°C and humidity: " +
+                                      str(sensorData.humidity)[:2] + "% at " + sensorData.timestamp)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.post("/orders/{order_id}/details")
 async def create_order_details(order_id: str, orderDetail: OrderDetail, background_tasks: BackgroundTasks):
     try:
@@ -75,6 +102,22 @@ async def create_order_details(order_id: str, orderDetail: OrderDetail, backgrou
             return response.json()
         else:
             raise HTTPException(status_code=500, detail="Failed to create order details in data-api")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.delete("/orders/{orderId}")
+async def delete_order(orderId: str):
+    try:
+        response = httpx.delete(f'http://localhost:8080/orders/{orderId}')
+
+        if response.status_code == 204:
+            for websocket in websockets:
+                await websocket.send_text("Refetch orders")
+            return response.status_code
+        else:
+            raise HTTPException(status_code=500, detail="Failed to delete order in data-api")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
